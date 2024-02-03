2488d27a8a2d97e59ae556b088ffb04c7810f235
Michael Foley <mpfoley73@hotmail.com>
2024-02-03 15:22:50 -0500

    First draft.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    author = 'Mike',
+    author_email = 'mpfoley73@hotmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
---
+++ src/prompt.py
@@ -0,0 +1,7 @@
+system_instruction = """
+Your are Zomato Orderbot, \
+and automated service to collect orders for an online restaurant. \
+Start by greeting the customer, then take their order, \
+and ask if they prefer pickup or delivery. If deivery \
+ask for their address.
+"""
